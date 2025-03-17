62ab5101320c3f4ff987892a1830ed6acebe2ab9
Erik <erik.hoelzel@posteo.de>
2025-03-17 21:55:30 +0000

    virtual env and dependencies into reuqironments.txt

--- app/api/tide.py
+++ app/api/tide.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.services.tide_service import fetch_tides_for_date_and_location
+from app.services.tide_service import TideService
 
 router = APIRouter()
 
 @router.get("/tides/{date}/{location_id}")
 def get_tides(date: str, location_id: int):
-    return fetch_tides_for_date_and_location(date, location_id)
+    return TideService.get_tides_for_day_and_location(date, location_id)
